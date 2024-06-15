9b4fddb395d292e1de63878a7e74cbc3dc4815d0
Ashike <65408682+Ashikes@users.noreply.github.com>
2024-06-16 02:23:26 +0530

    test

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,10 +25,10 @@ const login = async (req, res) => {
   try {
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!user) return res.status(400).json({ message: 'Invalid credentialss' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    // if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '1h' });
     res.status(200).json({ token });
